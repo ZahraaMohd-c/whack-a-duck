aa6b86891f397d8aaa5927413fbaba2558aa8210
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-07-09 11:40:56 +0300

    add duck class

--- script/app.js
+++ script/app.js
@@ -1,16 +1,22 @@
 function init (){
 
     const gridElm=document.querySelector('.grid')
+    const cells=[]
+    const gridwidth=10
+    const numberOfCells=gridwidth*gridwidth
+    
 
 function createGrid(){
 
    for(let i=0; i<100;i++){
     const cell =document.createElement('div')
+    cell.classList.add('duck')
     cell.textContent=i
+    cells.push(cell)
     gridElm.appendChild(cell)
 
    }
-  
+//   console.log(cells)
 }
   createGrid()
 }
