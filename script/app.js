8185e9b1053659e723c2f5f9837f8a93f0b83ac9
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-07-09 11:15:20 +0300

    create a grid

--- script/app.js
+++ script/app.js
@@ -1,4 +1,17 @@
 function init (){
 
+    const gridElm=document.querySelector('.grid')
+
+function createGrid(){
+
+   for(let i=0; i<100;i++){
+    const cell =document.createElement('div')
+    cell.textContent=i
+    gridElm.appendChild(cell)
+
+   }
+  
+}
+  createGrid()
 }
 document.addEventListener('DOMContentLoaded',init)
