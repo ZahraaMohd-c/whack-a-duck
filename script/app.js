1406648ebdc394bfd053a6bcd3f10ff24172755e
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-07-09 13:56:51 +0300

    add handle click and increment score

--- script/app.js
+++ script/app.js
@@ -4,20 +4,50 @@ function init (){
     const cells=[]
     const gridwidth=10
     const numberOfCells=gridwidth*gridwidth
-    
+    const scoreDisplayEl=document.querySelector('#score-display')
 
+
+    let duckposition = 10
+    let score=0
+function handleClick(event){
+    // console.log('it runs')
+    if(event.target.classList.contains('duck')){
+        score+=10
+        scoreDisplayEl.textContent='Your score is '+score
+
+    }
+}
 function createGrid(){
 
    for(let i=0; i<100;i++){
     const cell =document.createElement('div')
-    cell.classList.add('duck')
     cell.textContent=i
+    cell.addEventListener('click',handleClick)
     cells.push(cell)
     gridElm.appendChild(cell)
 
    }
-//   console.log(cells)
 }
+function addDuck(){
+    cells[duckposition].classList.add('duck')
+}
+
+function removeDuck(){
+    cells[duckposition].classList.remove('duck')
+}
+function play(){
+    setInterval(()=>{
+        removeDuck()
+        duckposition=Math.floor(Math.random()*numberOfCells)
+        addDuck()
+    },3000)
+
+}
+
+
+
   createGrid()
+  play()
+  
 }
 document.addEventListener('DOMContentLoaded',init)
