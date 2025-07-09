59d83f5508a3ddaadba362e17608ffc10fedcb77
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-07-09 14:46:57 +0300

    game is finished

--- script/app.js
+++ script/app.js
@@ -5,15 +5,20 @@ function init (){
     const gridwidth=10
     const numberOfCells=gridwidth*gridwidth
     const scoreDisplayEl=document.querySelector('#score-display')
+    const audioElm=document.querySelector('#quack')
 
 
     let duckposition = 10
     let score=0
+    let totalDucks=0
 function handleClick(event){
     // console.log('it runs')
     if(event.target.classList.contains('duck')){
+        audioElm.pause()
         score+=10
         scoreDisplayEl.textContent='Your score is '+score
+        audioElm.play()
+        removeDuck()
 
     }
 }
@@ -30,17 +35,33 @@ function createGrid(){
 }
 function addDuck(){
     cells[duckposition].classList.add('duck')
+    totalDucks++
 }
 
 function removeDuck(){
     cells[duckposition].classList.remove('duck')
 }
+
+function endGame(){
+    alert('Game ended, score: '+score)
+    score=0
+    scoreDisplayEl.textContent='Your score is '+score
+    totalDucks=0
+    
+
+}
 function play(){
     setInterval(()=>{
-        removeDuck()
-        duckposition=Math.floor(Math.random()*numberOfCells)
-        addDuck()
+        if(totalDucks<10){
+         removeDuck()
+         duckposition=Math.floor(Math.random()*numberOfCells)
+         addDuck()
+        }else{
+            endGame()
+        }
+        
     },3000)
+    
 
 }
 
